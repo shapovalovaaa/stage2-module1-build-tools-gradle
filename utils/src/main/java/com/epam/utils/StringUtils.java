2ba14ba062261b95f99a6af265bde2447d31c592
shapovalovaaa <nastyashpovalova70@gmail.com>
2023-08-06 15:31:05 +0300

    feat

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,11 @@
 package com.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
          //here magic will happen
+        return NumberUtils.isParsable(str) && str.charAt(0) != '0' &&
+                Double.parseDouble(str) > 0;
     }
 }
